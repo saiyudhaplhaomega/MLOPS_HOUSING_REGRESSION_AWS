8c8f6d05a8c58d81821ab1405890c7cb63a6bf62
Saiyudh Mannan <saiyudh.mannan@gmail.com>
2025-11-01 00:10:54 +0100

    fixing docker aws

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from pathlib import Path
 # Config
 # ============================
 API_URL = os.environ.get("API_URL", "http://127.0.0.1:8000/predict")
-S3_BUCKET = os.getenv("S3_BUCKET", "housing-regression-data")
-REGION = os.getenv("AWS_REGION", "eu-west-2")
+S3_BUCKET = os.getenv("S3_BUCKET", "housing-regression-data-sai")
+REGION = os.getenv("AWS_REGION", "eu-central-1")
 
 s3 = boto3.client("s3", region_name=REGION)
 
